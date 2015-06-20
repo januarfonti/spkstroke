2723e6d40f5c6e447c57872cf9490a17e41dc998
Januar Fonti <januarfonti@yahoo.com>
2015-06-20 14:55:18 +0700

    change db name

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,7 +2,7 @@
 	$host       = "localhost";
 	$username   = "root";
 	$password   = "";
-	$database   = "db_spkstroke";
+	$database   = "ruri_skripsi";
 	$connection = mysql_connect($host, $username, $password) or die("Kesalahan Koneksi ... !!");
 	mysql_select_db($database, $connection) or die("Databasenya Error");
 	$db 		=  new mysqli($host, $username, $password, $database);
